1b9058260351232faf1d873f88a5426f6155547a
NhatPhat <zzalanzz2409@gmail.com>
2025-11-03 16:41:34 +0700

    thêm Imageframe

--- frontend1/src/components/Navbar.jsx
+++ frontend1/src/components/Navbar.jsx
@@ -1,7 +1,4 @@
 import logo from '../images/Logo.png';
-import chaticon from '../images/Chat.png';
-import Heart from '../images/Heart.png';
-import Bookmark from '../images/Bookmark.png';
 import Bell from '../images/Bell.png';
 import Avatar from '../images/Avatar.png';
 export default function Navbar() {
@@ -13,27 +10,12 @@ export default function Navbar() {
         </a>
         <ul className="flex gap-6">
           <li>
-            <a href="#Matching">
-              <img src={Heart} alt="Matching" className="h-8 w-8 hover:opacity-80" />
-            </a>
-          </li>
-          <li>
-            <a href="#Chat">
-              <img src={chaticon} alt="Chat" className="h-8 w-8 hover:opacity-80" />
-            </a>
-          </li>
-           <li>
-            <a href="#Mark">
-              <img src={Bookmark} alt="Mark" className="h-8 w-8 hover:opacity-80" />
-            </a>
-          </li>
-          <li>
             <a href="#Bell">
               <img src={Bell} alt="Bell" className="h-8 w-8 hover:opacity-80" />
             </a>
           </li>
           <li>
-            <a href="#Avatar">
+            <a href="signup">
               <img src={Avatar} alt="Avatar" className="h-8 w-8 hover:opacity-80" />
             </a>
           </li>
--- frontend1/src/pages/LandingPage.jsx
+++ frontend1/src/pages/LandingPage.jsx
@@ -1,5 +1,8 @@
 import Navbar from "../components/Navbar";
 import SearchingBar from "../components/SearchingBar";
+import ImageFrame from "../components/ImageFrame";
+import condao from "../images/Con-Dao.jpg";
+import Hanoi from "../images/Hà Nội.jpg";
 
 export default function App() {
   return (
@@ -8,9 +11,26 @@ export default function App() {
       <div>
         <SearchingBar />
       </div>
-      <main className="p-6">
-        <p>Đây là phần nội dung chính của trang</p>
-      </main>
+     <main className="p-6 flex justify-center gap-2">
+  <ImageFrame
+    src={condao}
+    alt="Côn Đảo"
+    ratio="16/9"
+    rounded="xl"
+    shadow="lg"
+    className="w-[40%]"
+  />
+  <ImageFrame
+    src={Hanoi}
+    alt="Hà Nội"
+    ratio="16/9"
+    rounded="xl"
+    shadow="lg"
+    className="w-[40%]"
+  />
+</main>
+
+
     </div>
   );
 }     
