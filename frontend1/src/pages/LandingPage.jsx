cd681f37487fe9e0a8dfbcbba2a4d9fc9f79deb0
NhatPhat <zzalanzz2409@gmail.com>
2025-11-04 08:09:32 +0700

    Landingpage

--- frontend1/src/pages/LandingPage.jsx
+++ frontend1/src/pages/LandingPage.jsx
@@ -1,36 +1,77 @@
 import Navbar from "../components/Navbar";
 import SearchingBar from "../components/SearchingBar";
 import ImageFrame from "../components/ImageFrame";
-import condao from "../images/Con-Dao.jpg";
-import Hanoi from "../images/Hà Nội.jpg";
+
+import ConDao from "../images/Con-Dao.jpg";
+import HaNoi from "../images/Ha-Noi.jpg";      // nên tránh dấu & khoảng trắng trong tên file
+import CanTho from "../images/Can-Tho.jpg";
+import HoiAn from "../images/Hoi-An.jpg";
+import TPHCM from "../images/TPHCM.jpg";
 
 export default function App() {
   return (
-     <div>
+    <div>
       <Navbar />
       <div>
         <SearchingBar />
       </div>
-     <main className="p-6 flex justify-center gap-2">
-  <ImageFrame
-    src={condao}
-    alt="Côn Đảo"
-    ratio="16/9"
-    rounded="xl"
-    shadow="lg"
-    className="w-[40%]"
-  />
-  <ImageFrame
-    src={Hanoi}
-    alt="Hà Nội"
-    ratio="16/9"
-    rounded="xl"
-    shadow="lg"
-    className="w-[40%]"
-  />
-</main>
 
+      {/* main xếp theo cột */}
+      <main className="p-6 max-w-6xl mx-auto flex flex-col gap-4">
+         <p className="mb-6 ml-13 text-black text-4xl font-bold text-left">
+   Điểm đến thịnh hành
+  </p>
+        {/* HÀNG TRÊN: 2 ảnh to */}
+        <div className="flex justify-center gap-3">
+          <ImageFrame
+            src={ConDao}
+            alt="Côn Đảo"
+            ratio="21/9"
+            rounded="xl"
+            shadow="lg"
+            className="w-[45%]"
+          />
+          <ImageFrame
+            src={HaNoi}
+            alt="Hà Nội"
+            ratio="21/9"
+            rounded="xl"
+            shadow="lg"
+            className="w-[45%]"
+          />
+        </div>
 
+        {/* HÀNG DƯỚI: 3 ảnh nhỏ hơn */}
+   <div className="flex flex-nowrap justify-center gap-1.5 w-full">
+          <ImageFrame
+            src={TPHCM}
+            alt="Thành Phố Hồ Chí Minh"
+            ratio="21/9"
+            rounded="xl"
+            shadow="md"
+            className="w-full sm:w-[30%]"  /* nhỏ hơn hàng trên */
+          />
+          <ImageFrame
+            src={HoiAn}
+            alt="Hội An"
+            ratio="21/9"
+            rounded="xl"
+            shadow="md"
+            className="w-full sm:w-[30%]"
+          />
+          <ImageFrame
+            src={CanTho}
+            alt="Cần Thơ"
+            ratio="21/9"
+            rounded="xl"
+            shadow="md"
+            className="w-full sm:w-[30%]"
+          />
+        </div>
+         <p className="mt-9 ml-13 text-black text-4xl font-bold text-center">
+   Ưu đãi hôm nay
+  </p>
+      </main>
     </div>
   );
-}     
+}
