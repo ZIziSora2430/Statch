9298e6045a8e18ccbfa8362b026f811ed452296a
ZiziSora <nhkngan2430@clc.fitus.edu.vn>
2025-11-04 21:59:57 +0700

    Update database password in database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,7 +7,7 @@ def get_db_connection():
         conn = mysql.connector.connect(
             host="127.0.0.1",
             user="root",
-            password="na21",
+            password="Your password",
             database="statch_db"
         )
         if conn.is_connected(): 
